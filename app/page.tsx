817bf3773821ac9faaafac7f9840b4aa74d94ea9
jsaique <jsaique11@gmail.com>
2023-04-12 13:36:56 -0400

    Added Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Hero, HeroSubtitle, HeroTitle } from "@/components/hero";
 import { Container } from "../components/container";
 
 export default function Homepage() {
@@ -7,7 +8,17 @@ export default function Homepage() {
         <Container>header</Container>
       </header>
       <main>
-        <Container>Hero page</Container>
+        <Container>
+          <Hero>
+            <HeroTitle>John Aries Saique</HeroTitle>
+            <HeroSubtitle>
+              I am a self-taught frontend developer who is passionate about
+              <br />
+              solving problems and learning new skills.
+            </HeroSubtitle>
+          </Hero>
+          <img src="/img/code.jpg" alt="" />
+        </Container>
       </main>
       <footer>
         <Container>footer</Container>
