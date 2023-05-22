2d1a35914d3449ea1499c11ddf0489f2c1d0b0b0
jsaique <jsaique11@gmail.com>
2023-05-21 21:45:31 -0400

    Added logos

--- app/skills.tsx
+++ app/skills.tsx
@@ -1,1 +1,44 @@
-export const Skills = () => <>Hello Skills!!!</>;
+import { Flask } from "@/components/logos/flask";
+import { JavaScript } from "@/components/logos/js";
+import { NextJs } from "@/components/logos/nextjs";
+import { Python } from "@/components/logos/python";
+import { React } from "@/components/logos/react";
+import { Sql } from "@/components/logos/sql";
+import { TypeScript } from "@/components/logos/typescript";
+
+export const Skills = () => (
+  <>
+    <p>My Skills</p>
+    <p>Tech Stack</p>
+    <div className="flex flex-wrap justify-around gap-x-6 gap-y-8 [&_svg]:max-w-[16rem]">
+      <div className="flex flex-col items-center">
+        <JavaScript className="w-8 h-8" />
+        <p>JavaScript</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <NextJs className="w-8 h-8" />
+        <p>NextJS</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <Python className="w-8 h-8" />
+        <p>Python</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <Flask className="w-8 h-8" />
+        <p>Flask</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <React className="w-8 h-8" />
+        <p>React</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <Sql className="w-8 h-8" />
+        <p>SQL</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <TypeScript className="w-8 h-8" />
+        <p>TypeScript</p>
+      </div>
+    </div>
+  </>
+);
--- components/logos/python.tsx
+++ components/logos/python.tsx
@@ -1,12 +1,11 @@
 export const Python = ({ className }: { className?: string }) => (
   <svg
+    className={className}
     stroke="currentColor"
     fill="currentColor"
     strokeWidth="0"
     viewBox="0 0 24 24"
     role="img"
-    height="1em"
-    width="1em"
     xmlns="http://www.w3.org/2000/svg"
   >
     <title></title>
